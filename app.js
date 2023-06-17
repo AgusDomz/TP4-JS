99a7d4794184656a75b1f5df4077b786f9c408eb
agusdomz <agusdominguez95@gmail.com>
2023-06-17 20:25:52 -0300

    trabajo terminado

--- app.js
+++ app.js
@@ -171,7 +171,6 @@ Por último, utilice el método imprime datos para mostrar por pantalla los valo
 
 */
 
-
 /*
 5- Crea una clase llamada Persona que siga las siguientes condiciones:
 Sus propiedades son: nombre, edad, DNI, sexo (H hombre, M mujer), peso y altura, año de nacimiento. Si quieres añadir alguna propiedad extra puedes hacerlo.
@@ -324,3 +323,235 @@ Por último, indicar cuál de los 2 objetos “libros” tiene más páginas.
 
 */
 
+/*
+7- Nos piden realizar una agenda telefónica de contactos.
+
+Un contacto está definido por un nombre y un teléfono. Se considera que un contacto es igual a otro cuando sus nombres son iguales.
+
+Una agenda de contactos está formada por un conjunto de contactos. Se podrá crear de dos formas, indicando nosotros el tamaño o con un tamaño por defecto (10).
+
+Los métodos de la agenda serán los siguientes:
+
+aniadirContacto(Contacto): Añade un contacto a la agenda, sino la agenda no puede almacenar más contactos indicar por pantalla.
+existeContacto(Contacto): indica si el contacto pasado existe o no.
+listarContactos(): Lista toda la agenda
+buscarContacto(nombre): busca un contacto por su nombre y muestra su teléfono.
+eliminarContacto(Contacto c): elimina el contacto de la agenda, indica si se ha eliminado o no por pantalla
+agendaLlena(): indica si la agenda está llena.
+huecosLibres(): indica cuántos contactos más podemos ingresar.
+
+Crea un menú con opciones que serán seleccionadas por el usuario usando un prompt, las salidas de las operaciones seleccionadas por el usuario se pueden mostrar en pantalla y  por consola.
+*/
+/*
+    class Contacto {
+        constructor(nombre, telefono) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        }
+
+        otrocontact(otroContacto) {
+        return this.nombre.toLowerCase() === otroContacto.nombre.toLowerCase();
+        }
+    }
+
+    class Agenda {
+        constructor(tamano = 10) {
+        this.contactos = [];
+        this.tamano = tamano;
+        }
+
+        aniadirContacto(contacto) {
+        if (this.agendaLlena()) {
+            console.log("La agenda está llena. ");
+            return;
+        }
+
+        if (this.existeContacto(contacto)) {
+            console.log("El contacto ya existe");
+            return;
+        }
+
+        this.contactos.push(contacto);
+        console.log("Contacto añadido");
+        }
+
+        existeContacto(contacto) {
+        return this.contactos.some((c) => c.otrocontact(contacto));
+        }
+
+        listarContactos() {
+        console.log("Lista de contactos en la agenda:");
+        this.contactos.forEach((contacto) => {
+            console.log(
+            "Nombre: " + contacto.nombre + ", Teléfono: " + contacto.telefono
+            );
+        });
+        }
+
+        buscarContacto(nombre) {
+        const contactosEncontrados = this.contactos.filter((contacto) =>
+            contacto.nombre.toLowerCase().includes(nombre.toLowerCase())
+        );
+
+        if (contactosEncontrados.length === 0) {
+            console.log("No se encontró ningún contacto con ese nombre.");
+        } else {
+            console.log("Contactos encontrados:");
+            contactosEncontrados.forEach((contacto) => {
+            console.log(
+                "Nombre: " + contacto.nombre + ", Teléfono: " + contacto.telefono
+            );
+            });
+        }
+        }
+
+        eliminarContacto(contacto) {
+        const index = this.contactos.findIndex((c) => c.otrocontact(contacto));
+
+        if (index === -1) {
+            console.log("El contacto no existe en la agenda.");
+        } else {
+            this.contactos.splice(index, 1);
+            console.log("Contacto eliminado correctamente.");
+        }
+        }
+
+        agendaLlena() {
+        return this.contactos.length >= this.tamano;
+        }
+
+        huecosLibres() {
+        return this.tamano - this.contactos.length;
+        }
+    }
+
+    
+    var agenda = new Agenda();
+
+
+    while (true) {
+        var opcion = prompt(
+            "Selecciona una opción:\n1. Añadir contacto\n2. Buscar contacto\n3.     Eliminar    contacto\n4. Listar contactos\n5. Salir"
+        );
+
+        switch (opcion) {
+        case "1":
+            var nombre = prompt("Ingrese el nombre del contacto:");
+            var telefono = prompt("Ingrese el teléfono del contacto:");
+            var nuevoContacto = new Contacto(nombre, telefono);
+            agenda.añadirContacto(nuevoContacto);
+            break;
+
+        case "2":
+            var nombreBuscar = prompt("Ingrese el nombre del contacto a buscar:");
+            agenda.buscarContacto(nombreBuscar);
+            break;
+
+        case "3":
+            var nombreEliminar = prompt("Ingrese el nombre del contacto a eliminar:");
+            var contactoEliminar = new Contacto(nombreEliminar, "");
+            agenda.eliminarContacto(contactoEliminar);
+            break;
+
+        case "4":
+            agenda.listarContactos();
+            break;
+
+        case "5":
+            console.log("see you");
+            return;
+        }
+    }
+/*
+/*
+8- Crea una clase llamada "Persona" que tenga las propiedades "nombre", "edad" y "profesión", y los métodos "saludar" y "despedirse". Luego, crea dos objetos de la clase "Persona" con diferentes valores para sus propiedades y llama a sus métodos "saludar" y "despedirse".
+*/
+
+/*
+    class Persona {
+        constructor(nombre, edad, profesion){
+            this.nombre = nombre;
+            this.edad = edad;
+            this.profesion = profesion;
+        }
+
+        saludar(){
+            return `Hola ${this.profesion}, ${this.nombre}`;
+        }
+
+        despedirse(){
+            return `Chau ${this.profesion}, ${this.nombre}`;
+        }
+    }
+
+    const persona1 = new Persona ("Agustin", 23, "Doctor");
+    const persona2 = new Persona ("Pablo", 32, "Ingeniero");
+
+    console.log(persona1.saludar());
+
+    console.log(persona2.despedirse());
+*/
+
+/*
+9- Crea una clase llamada "Animal" que tenga las propiedades "nombre" y "edad", y el método "emitirSonido". Luego, crea dos clases hijas llamadas "Perro" y "Gato" que hereden de "Animal" y tengan su propio método "emitirSonido". Finalmente, crea dos objetos, uno de la clase "Perro" y otro de la clase "Gato", y llama a sus métodos "emitirSonido" para verificar que cada animal emite el sonido adecuado. 
+*/
+/*
+    class Animal {
+        constructor(nombre, edad){
+            this.nombre = nombre;
+            this.edad = edad;
+        }
+
+        emitirSonido(){
+            console.log("El animal emite un sonido");
+        }
+    }
+
+    class Perro extends Animal{
+        emitirSonido() {
+            console.log(`Mi perro ${this.nombre} hace: Guau!`);
+        }
+    }
+
+    class Gato extends Animal {
+        emitirSonido(){
+            console.log(`Mi gata ${this.nombre} hace: Miau!`);
+        }
+    }
+
+    const miPerro = new Perro ("Coco", 4);  
+    const miGato = new Gato ("Nicky", 1); 
+
+    miPerro.emitirSonido();
+    miGato.emitirSonido();
+*/
+
+/*
+10- crear una clase aeropuerto con las propiedades nombreAeropuerto y lista de aviones, esta clase deberá contener el método agregarAvion, el cual recibirá un objeto de tipo Avión, además de un método buscarAvion el cual recibirá el nombre de un avión y devolverá información en caso de encontrarlo, si no lo encontró indicar con un mensaje.
+Por su parte los aviones tendrán las propiedades: nombre, capacidad, destino, lista de pasajeros. Los aviones tienen el método abordar el cual permite que un pasajero suba al avión solo si hay capacidad disponible en el mismo, caso contrario mostrar un mensaje que indique que el avión está lleno.
+
+Crear un objeto de tipo aeropuerto llamado "Aeropuerto Internacional", crear 3 objetos aviones con diferentes destinos. Agregar los 3 aviones al aeropuerto, buscar un avión y usar el método abordar.
+ */
+
+class Avion {
+  constructor(nombre, capacidad, destino, listaPasajeros) {
+    this.nombre = nombre;
+    this.capacidad = capacidad;
+    this.destino = destino;
+    this.listaPasajeros = [];
+  }
+
+  abordar() {}
+}
+
+class Aeropuerto {
+  constructor(nombreAeropuerto, listaAviones) {
+    this.nombreAeropuerto = nombreAeropuerto;
+    this.listaAviones = [];
+  }
+
+  agregarAvion(avion) {
+    this.listaAviones.push(avion);
+    console.log(`El avion `);
+  }
+}
